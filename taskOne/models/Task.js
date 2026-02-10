c52944c2c7345922929a9f3cce090a3d6c10d5dc
‪Elsayed Elshaerawy‬‏ <107215225+Elsayed009@users.noreply.github.com>
2026-02-10 20:19:41 +0200

    Remove commented code and clean Task.js

--- taskOne/models/Task.js
+++ taskOne/models/Task.js
@@ -22,63 +22,8 @@ const taskSchema = new mongoose.Schema({
 const Task = mongoose.model("Task", taskSchema)
 module.exports= Task;
 
-// //require models
-// const User = require ("./model/user");
-// const Post = require("./model/Post");
-// // Middleware to parse JSON request bodies
-// app.post("/users", async (req, res)=>{
-//     try {
-//         const user =await User.create(req.body)
-        
-//         console.log(req.body)
-//         res.json({
-//             msg: "created done",
-//             data: user
-//         });
-//         res.send("hi user")
-//     }catch (error) {
-//         // res.status(500).json({message: "Server error"});
-//         console.log("error: ", error.message)
-//     }
-// });
-
-// app.post("/posts", async (req, res)=>{
-//     try {
-//        // get data from req.body
-//        const {title, content} = req.body;
-//     //    const title = req.body.title;
-//         // return console.log(req);
-//         // return console.log(req.body);
-//        // insert data to db
-//        const post = await Post.create({
-//         title,
-//         content,
-//         user: req.body.userId
-//        });
-//        res.json({
-//         msg: "post created",
-//         data: post
-//        });
-
-//     }catch (error) {
-//         // res.status(500).json({message: "Server error"});
-//         console.log("error: ", error.message)
-//     }
-// });
 
 
-// app.get( "/posts", async (req, res)=>{
-//     try{
-//         const post = await Post.find().populate("user", "username");
-//         res.json({
-//             data: post,
-
-//         });
-//     }catch{
-        
-
-//     };
-// })
 
 
 
